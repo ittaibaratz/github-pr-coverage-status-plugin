306a444cee4fc150548cc391b4b8ed0a0d8ef656
cyril-sebastian <cyril.sebastian@clarabridge.com>
2021-05-30 21:48:36 -0500

    CB-12323 configurable coverage report

---
+++ src/main/java/com/github/terma/jenkins/githubprcoveragestatus/ReportData.java
@@ -0,0 +1,51 @@
+package com.github.terma.jenkins.githubprcoveragestatus;
+
+public class ReportData {
+    private int covered;
+    private int total;
+
+    public ReportData() {
+        this.covered = 0;
+        this.total = 0;
+    }
+
+    public ReportData(int covered, int total) {
+        this.covered = covered;
+        this.total = total;
+    }
+
+    public void add(ReportData other) {
+        this.covered += other.covered;
+        this.total += other.total;
+    }
+
+    public float getRate() {
+        return this.covered / this.total;
+    }
+
+    @Override
+    public String toString() {
+        return "ReportData{" +
+                "covered=" + covered +
+                ", total=" + total +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReportData that = (ReportData) o;
+
+        if (covered != that.covered) return false;
+        return total == that.total;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = covered;
+        result = 31 * result + total;
+        return result;
+    }
+}
